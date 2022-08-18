ae744710e596e4ba960e798be377d77dca5c183e
Juha Kukkonen <juha7kukkonen@gmail.com>
2022-08-19 01:15:06 +0300

    config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -121,8 +121,6 @@ require("dressing").setup({
 })
 require("trouble").setup({})
 
-require("alpha").setup(require("alpha.themes.dashboard").config)
-
 local alpha = require("alpha")
 local dashboard = require("alpha.themes.dashboard")
 
