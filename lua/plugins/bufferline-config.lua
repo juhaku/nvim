433acdaeeea09321b00a5e31c1b74b630fcc8212
Juha Kukkone <juha7kukkonen@gmail.com>
2022-10-19 09:53:14 +0300

    update bufferline max name lenght

--- lua/plugins/bufferline-config.lua
+++ lua/plugins/bufferline-config.lua
@@ -4,7 +4,7 @@ require("bufferline").setup({
 		mode = "tabs",
 		show_close_icon = false,
 		-- show_buffer_close_icons = false,
-		max_name_length = 25,
+		max_name_length = 50,
 		-- max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
 		-- truncate_names = true, -- whether or not tab names should be truncated
 		-- tab_size = 18,
