9e0e66484632c973e013e06db45f367d85bc2a4c
Juha Kukkonen <juha7kukkonen@gmail.com>
2023-02-09 20:00:58 +0200

    Update revert dap ui

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,85 +1,86 @@
 require("packer").startup(function()
-	use("wbthomason/packer.nvim")
+    use("wbthomason/packer.nvim")
 
-	use("lewis6991/impatient.nvim")
-	use("folke/tokyonight.nvim")
+    use("lewis6991/impatient.nvim")
+    use("folke/tokyonight.nvim")
 
-	use("kyazdani42/nvim-web-devicons")
-	use({
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v2.x",
-		requires = {
-			"nvim-lua/plenary.nvim",
-			"kyazdani42/nvim-web-devicons",
-			"MunifTanjim/nui.nvim",
-		},
-	})
+    use("kyazdani42/nvim-web-devicons")
+    use({
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v2.x",
+        requires = {
+            "nvim-lua/plenary.nvim",
+            "kyazdani42/nvim-web-devicons",
+            "MunifTanjim/nui.nvim",
+        },
+    })
 
-	use("tpope/vim-commentary")
-	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-	use("nvim-treesitter/nvim-treesitter-refactor")
-	use("nvim-treesitter/nvim-treesitter-context")
-	use("nvim-lualine/lualine.nvim")
-	use("p00f/nvim-ts-rainbow")
-	use("nvim-telescope/telescope.nvim")
-	use("nvim-telescope/telescope-file-browser.nvim")
-	use("windwp/nvim-ts-autotag")
-	use("windwp/nvim-autopairs")
-	use("lewis6991/gitsigns.nvim")
-	use("SmiteshP/nvim-navic")
-	use("stevearc/dressing.nvim")
-	use("folke/trouble.nvim")
-	use("lukas-reineke/indent-blankline.nvim")
-	use("https://gitlab.com/yorickpeterse/nvim-pqf")
-	use("phaazon/hop.nvim")
-	use("RRethy/vim-illuminate")
-	use("jose-elias-alvarez/null-ls.nvim")
-	use("kylechui/nvim-surround")
-	use("nvim-lua/plenary.nvim")
-	-- use("j-hui/fidget.nvim")
-	use("folke/lsp-colors.nvim")
-	use({ "akinsho/bufferline.nvim", tag = "v3.*" })
-	use({ "mbbill/undotree" })
+    use("tpope/vim-commentary")
+    use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+    use("nvim-treesitter/nvim-treesitter-refactor")
+    use("nvim-treesitter/nvim-treesitter-context")
+    use("nvim-lualine/lualine.nvim")
+    use("p00f/nvim-ts-rainbow")
+    use("nvim-telescope/telescope.nvim")
+    use("nvim-telescope/telescope-file-browser.nvim")
+    use("windwp/nvim-ts-autotag")
+    use("windwp/nvim-autopairs")
+    use("lewis6991/gitsigns.nvim")
+    use("SmiteshP/nvim-navic")
+    use("stevearc/dressing.nvim")
+    use("folke/trouble.nvim")
+    use("lukas-reineke/indent-blankline.nvim")
+    use("https://gitlab.com/yorickpeterse/nvim-pqf")
+    use("phaazon/hop.nvim")
+    use("RRethy/vim-illuminate")
+    use("jose-elias-alvarez/null-ls.nvim")
+    use("kylechui/nvim-surround")
+    use("nvim-lua/plenary.nvim")
+    -- use("j-hui/fidget.nvim")
+    use("folke/lsp-colors.nvim")
+    use({ "akinsho/bufferline.nvim", tag = "v3.*" })
+    use({ "mbbill/undotree" })
 
-	-- cmp plugins
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-path")
-	use("hrsh7th/cmp-cmdline")
-	use("hrsh7th/nvim-cmp")
-	use("onsails/lspkind.nvim")
+    -- cmp plugins
+    use("hrsh7th/cmp-nvim-lsp")
+    use("hrsh7th/cmp-buffer")
+    use("hrsh7th/cmp-path")
+    use("hrsh7th/cmp-cmdline")
+    use("hrsh7th/nvim-cmp")
+    use("onsails/lspkind.nvim")
 
-	-- lua snippets
-	use("L3MON4D3/LuaSnip")
-	use("saadparwaiz1/cmp_luasnip")
+    -- lua snippets
+    use("L3MON4D3/LuaSnip")
+    use("saadparwaiz1/cmp_luasnip")
 
-	use("williamboman/mason.nvim")
-	use("williamboman/mason-lspconfig.nvim")
-	use("neovim/nvim-lspconfig")
+    use("williamboman/mason.nvim")
+    use("williamboman/mason-lspconfig.nvim")
+    use("neovim/nvim-lspconfig")
 
-	-- dap
-	use("mfussenegger/nvim-dap")
-	use("theHamsta/nvim-dap-virtual-text")
-	use("rcarriga/nvim-dap-ui")
-	use("leoluz/nvim-dap-go")
-	use("mxsdev/nvim-dap-vscode-js")
+    -- dap
+    use("mfussenegger/nvim-dap")
+    use("theHamsta/nvim-dap-virtual-text")
+    -- temporary "fix" the error: https://github.com/rcarriga/nvim-dap-ui/issues/224
+    use({ "rcarriga/nvim-dap-ui", commit = "b0612c9177c76a24da2a15feea6dbc3d742ec95c" })
+    use("leoluz/nvim-dap-go")
+    use("mxsdev/nvim-dap-vscode-js")
 
-	-- languages
-	use("simrat39/rust-tools.nvim")
-	use("saecki/crates.nvim")
-	use("crispgm/nvim-go")
-	use("mfussenegger/nvim-jdtls")
-	use("akinsho/flutter-tools.nvim")
-	use("b0o/schemastore.nvim")
-	use("jose-elias-alvarez/typescript.nvim")
+    -- languages
+    use("simrat39/rust-tools.nvim")
+    use("saecki/crates.nvim")
+    use("crispgm/nvim-go")
+    use("mfussenegger/nvim-jdtls")
+    use("akinsho/flutter-tools.nvim")
+    use("b0o/schemastore.nvim")
+    use("jose-elias-alvarez/typescript.nvim")
 
-	-- dashboard & sessions
-	use("goolord/alpha-nvim")
-	use("Shatur/neovim-session-manager")
+    -- dashboard & sessions
+    use("goolord/alpha-nvim")
+    use("Shatur/neovim-session-manager")
 
-	-- git
-	use("sindrets/diffview.nvim")
-	use("tpope/vim-fugitive")
+    -- git
+    use("sindrets/diffview.nvim")
+    use("tpope/vim-fugitive")
 end)
 
 local autopairs = require("nvim-autopairs")
@@ -92,31 +93,31 @@ autopairs.setup({})
 
 require("nvim-surround").setup({})
 require("dressing").setup({
-	input = {
-		win_options = {
-			-- Window transparency (0-100)
-			winblend = 20,
-			-- Change default highlight groups (see :help winhl)
-			-- winhighlight = "NormalFloat guibg=NONE",
-		},
-	},
-	select = {
-		enabled = true,
-		winblend = 20,
-		backend = { "telescope", "fzf_lua", "fzf", "builtin", "nui" },
+    input = {
+        win_options = {
+            -- Window transparency (0-100)
+            winblend = 20,
+            -- Change default highlight groups (see :help winhl)
+            -- winhighlight = "NormalFloat guibg=NONE",
+        },
+    },
+    select = {
+        enabled = true,
+        winblend = 20,
+        backend = { "telescope", "fzf_lua", "fzf", "builtin", "nui" },
 
-		get_config = function(opts)
-			if opts.kind == "codeaction" then
-				return {
-					backend = "builtin",
-					builtin = {
-						min_height = { 0., 0. },
-						relative = "cursor",
-					},
-				}
-			end
-		end,
-	},
+        get_config = function(opts)
+            if opts.kind == "codeaction" then
+                return {
+                    backend = "builtin",
+                    builtin = {
+                        min_height = { 0., 0. },
+                        relative = "cursor",
+                    },
+                }
+            end
+        end,
+    },
 })
 require("trouble").setup({})
 
@@ -124,38 +125,38 @@ local alpha = require("alpha")
 local dashboard = require("alpha.themes.dashboard")
 
 dashboard.section.buttons.val = {
-	dashboard.button("e", "  New file", ":ene <BAR> startinsert<CR>"),
-	dashboard.button("tf", "  Find file", ":Telescope find_files<CR>"),
-	dashboard.button("tg", "  Find text", ":Telescope live_grep<CR>"),
-	dashboard.button("tr", "  Recently used files", ":Telescope oldfiles<CR>"),
-	dashboard.button("te", "  Open file browser", ":Telescope file_browser<CR>"),
-	-- dashboard.button("te", "  Open file explorer", ":Ex<CR>"),
-	dashboard.button("os", "  Open session", ":SessionManager load_session<CR>"),
-	dashboard.button("ol", "  Open last session", ":SessionManager load_last_session<CR>"),
-	dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua<CR>"),
-	dashboard.button("q", "  Quit NVIM", ":qa<CR>"),
+    dashboard.button("e", "  New file", ":ene <BAR> startinsert<CR>"),
+    dashboard.button("tf", "  Find file", ":Telescope find_files<CR>"),
+    dashboard.button("tg", "  Find text", ":Telescope live_grep<CR>"),
+    dashboard.button("tr", "  Recently used files", ":Telescope oldfiles<CR>"),
+    dashboard.button("te", "  Open file browser", ":Telescope file_browser<CR>"),
+    -- dashboard.button("te", "  Open file explorer", ":Ex<CR>"),
+    dashboard.button("os", "  Open session", ":SessionManager load_session<CR>"),
+    dashboard.button("ol", "  Open last session", ":SessionManager load_last_session<CR>"),
+    dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua<CR>"),
+    dashboard.button("q", "  Quit NVIM", ":qa<CR>"),
 }
 
 require("session_manager").setup({
-	autoload_mode = require("session_manager.config").AutoloadMode.Disabled,
+    autoload_mode = require("session_manager.config").AutoloadMode.Disabled,
 })
 
 alpha.setup(dashboard.config)
 
 require("crates").setup({
-	null_ls = {
-		enabled = true,
-	},
+    null_ls = {
+        enabled = true,
+    },
 })
 
 require("indent_blankline").setup({
-	show_current_context = true,
-	show_current_context_start = false,
-	indent_blankline_use_treesitter_scope = true,
+    show_current_context = true,
+    show_current_context_start = false,
+    indent_blankline_use_treesitter_scope = true,
 })
 
 require("pqf").setup({
-	signs = { Error = "", Warn = "", Hint = "", Info = "" },
+    signs = { Error = "", Warn = "", Hint = "", Info = "" },
 })
 
 require("hop").setup()
