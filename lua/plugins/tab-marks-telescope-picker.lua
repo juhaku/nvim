34f21d3eb7ce89898e4d3a9f351ee9c3cb7f917d
Juha Kukkonen <juha7kukkonen@gmail.com>
2023-11-05 22:06:03 +0200

    Update tab telescope selectors
    Move tab selector to own file and create tab mark selector with
    possibility to mark tabs with index number from 1 to 5. The tab marks
    will be persisted over restarts. Add support for sorting tabs in tab
    selector by last usage.

---
+++ lua/plugins/tab-marks-telescope-picker.lua
@@ -0,0 +1,259 @@
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local conf = require("telescope.config").values
+local themes = require("telescope.themes")
+local telescope_actions = require("telescope.actions")
+local telescope_actions_state = require("telescope.actions.state")
+local tabs = require("plugins.tabs-telescope-picker")
+
+local keymap_opts = { noremap = true, silent = true }
+
+local home = os.getenv("HOME")
+local config_file = home .. "/.config/nvim/lib/tabmarks.json"
+
+---@class TabMark
+---@field tab TabResult|nil tab result attached to the index.
+
+---@type TabMark[]
+local _marks = {
+	{},
+	{},
+	{},
+	{},
+	{},
+}
+
+---Find `TabMark` from `marks` by `bufnr` of the tab buffer
+---@param bufnr number buffer number of the tab window
+---@param marks TabMark[] tab results to look for buffer
+---@return number|nil index Found tab index or `nil` if `results` does not have suchs buffer
+---@return TabResult|nil result Found tab result or `nil` if `results` does not have such buffer
+local function find_tab_mark_by_bufnr(bufnr, marks)
+	local i, found = nil, nil
+	for index, tab in ipairs(marks) do
+		if tab.tab ~= nil and tab.tab.bufnr == bufnr then
+			i = index
+			found = tab
+			break
+		end
+	end
+
+	return i, found
+end
+
+---Generate new tab marks finder
+---@param marks TabMark[] marks to use in the finder
+---@param cwdlen number length of the current working directory
+---@return table finder for the tab marks telescope
+local function generate_finder(marks, cwdlen)
+	local m = {}
+	for index, mark in ipairs(marks) do
+		table.insert(m, table.pack(index, mark))
+	end
+
+	return finders.new_table({
+		results = m,
+		entry_maker = function(marks_list)
+			local i, mark = table.unpack(marks_list)
+			if mark.tab ~= nil then
+				local name = string.sub(mark.tab.bufname, cwdlen + 1)
+
+				return {
+					value = mark,
+					display = "#" .. i .. " (" .. mark.tab.tabnr .. ") ." .. name,
+					ordinal = mark.tab.bufname,
+				}
+			else
+				return {
+					value = "",
+					display = "None",
+					ordinal = "None",
+				}
+			end
+		end,
+	})
+end
+
+---Delete tab mark from the tab marks picker and refresh the picker
+---@param prompt_bufnr number telescope prompt buffer number
+---@param cwdlen number length of the path of current working directory
+local function delete_tab_mark(prompt_bufnr, cwdlen)
+	local entry = telescope_actions_state.get_selected_entry()
+	if entry ~= nil and entry.value ~= "" then
+		table.remove(_marks, entry.index)
+		table.insert(_marks, entry.index, {})
+		telescope_actions_state
+			.get_current_picker(prompt_bufnr)
+			:refresh(generate_finder(_marks, cwdlen), { reset_prompt = true })
+	end
+end
+
+---Move tab mark down in the tab marks picker
+---@param prompt_bufnr number telescope prompt buffer number
+---@param cwdlen number length of the path of current working directory
+local function move_tab_mark_down(prompt_bufnr, cwdlen)
+	local entry = telescope_actions_state.get_selected_entry()
+
+	local index = entry.index
+	local len = #_marks
+
+	if index < len then
+		local current = _marks[index]
+
+		table.remove(_marks, index)
+		table.insert(_marks, index + 1, current)
+		local current_picker = telescope_actions_state.get_current_picker(prompt_bufnr)
+		local selection = current_picker:get_selection_row()
+		print("selection:" .. tostring(selection))
+
+		current_picker:refresh(generate_finder(_marks, cwdlen), { reset_prompt = true })
+		telescope_actions.move_selection_next(prompt_bufnr)
+		-- current_picker:set_selection(selection + 1)
+	end
+end
+
+---Move tab mark up in the tab marks picker
+---@param prompt_bufnr number telescope prompt buffer number
+---@param cwdlen number length of the path of current working directory
+local function move_tab_mark_up(prompt_bufnr, cwdlen)
+	local entry = telescope_actions_state.get_selected_entry()
+
+	local index = entry.index
+
+	if index > 1 then
+		local previous = _marks[index - 1]
+
+		table.remove(_marks, index - 1)
+		table.insert(_marks, index, previous)
+		telescope_actions_state
+			.get_current_picker(prompt_bufnr)
+			:refresh(generate_finder(_marks, cwdlen), { reset_prompt = true })
+	end
+end
+
+local function tab_marks_picker(opts)
+	opts = vim.tbl_deep_extend("force", {
+		initial_mode = "normal",
+	}, themes.get_dropdown(opts or {}))
+	local cwdlen = #vim.fn.getcwd()
+
+	pickers
+		.new(opts, {
+			prompt_title = "Tab Marks",
+			-- selection_strategy = "follow",
+			finder = generate_finder(_marks, cwdlen),
+			sorter = conf.generic_sorter(opts),
+			attach_mappings = function(prompt_bufnr, map) -- map
+				telescope_actions.select_default:replace(function()
+					telescope_actions.close(prompt_bufnr)
+					local entry = telescope_actions_state.get_selected_entry()
+					if entry ~= nil and entry.value ~= "" then
+						local tab = entry.value.tab.tabnr
+						vim.cmd("tabnext " .. tab)
+
+						if entry.value.tab.window ~= nil then
+							vim.fn.win_gotoid(entry.value.tab.window)
+						end
+					end
+				end)
+
+				map("n", "d", function()
+					delete_tab_mark(prompt_bufnr, cwdlen)
+				end)
+				map("i", "<C-d>", function()
+					delete_tab_mark(prompt_bufnr, cwdlen)
+				end)
+				map("n", "J", function()
+					-- down
+					move_tab_mark_down(prompt_bufnr, cwdlen)
+				end)
+				map("n", "K", function()
+					-- up
+					move_tab_mark_up(prompt_bufnr, cwdlen)
+				end)
+
+				return true
+			end,
+		})
+		:find()
+end
+
+vim.keymap.set("n", "<leader>m", function()
+	local bufnr = vim.api.nvim_get_current_buf()
+
+	local _results = tabs.get_tab_results()
+
+	vim.ui.input({ prompt = "Mark tab index # 1-5: " }, function(input)
+		local idx = tonumber(input)
+
+		local _, tab = tabs.find_tab_result_by_bufnr(bufnr, _results)
+		local _, mark = find_tab_mark_by_bufnr(bufnr, _marks)
+
+		if tab ~= nil and idx ~= nil and mark == nil then
+			table.remove(_marks, idx)
+			table.insert(_marks, idx, {
+				tab = tab,
+			})
+		end
+	end)
+end, keymap_opts)
+
+vim.keymap.set("n", "<leader>tm", function()
+	tab_marks_picker()
+end, keymap_opts)
+
+---Select mark by marks `index` number
+---@param index integer marks index number
+local function select_mark(index)
+	local mark = _marks[index]
+	if mark.tab ~= nil then
+		local tab = mark.tab.tabnr
+		vim.cmd("tabnext " .. tab)
+
+		if mark.tab.window ~= nil then
+			vim.fn.win_gotoid(mark.tab.window)
+		end
+	end
+end
+
+vim.keymap.set("n", "<leader>1", function()
+	select_mark(1)
+end, keymap_opts)
+vim.keymap.set("n", "<leader>2", function()
+	select_mark(2)
+end, keymap_opts)
+vim.keymap.set("n", "<leader>3", function()
+	select_mark(3)
+end, keymap_opts)
+vim.keymap.set("n", "<leader>4", function()
+	select_mark(4)
+end, keymap_opts)
+vim.keymap.set("n", "<leader>5", function()
+	select_mark(5)
+end, keymap_opts)
+
+local function save_config()
+	local json = vim.fn.json_encode(_marks)
+
+	local cmd = "echo '" .. json .. "' > " .. config_file
+
+	vim.fn.system(cmd)
+end
+
+vim.api.nvim_create_autocmd({ "VimLeavePre" }, {
+	pattern = { "*" },
+	callback = save_config,
+})
+
+local function read_config()
+	local is_config_file = vim.fn.system("test -f " .. config_file .. " && echo 1")
+	if tonumber(is_config_file) == 1 then
+		local json_str = vim.fn.system("cat " .. config_file)
+		_marks = vim.fn.json_decode(json_str)
+	end
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+	pattern = { "*" },
+	callback = read_config,
+})
---
+++ lua/plugins/tabs-telescope-picker.lua
@@ -0,0 +1,247 @@
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local conf = require("telescope.config").values
+local themes = require("telescope.themes")
+local telescope_actions = require("telescope.actions")
+local telescope_actions_state = require("telescope.actions.state")
+
+local keymap_opts = { noremap = true, silent = true }
+local tab_picker_default_settings = { settings = { all_buffers = true } }
+local tab_picker_sort_last_active = 1
+
+local M = {}
+
+--- Get active buffer number withing given tab
+---@param tab_num number tab number to get active buffer number for
+---@return number bufnr active buffer number in tab; or -1 if tab is not valid
+M.get_active_buf_for_tab = function(tab_num)
+	local is_valid = vim.api.nvim_tabpage_is_valid(tab_num)
+
+	if is_valid == true then
+		local window = vim.api.nvim_tabpage_get_win(tab_num)
+		return vim.api.nvim_win_get_buf(window)
+	else
+		return -1
+	end
+end
+
+--- Get buffer name for given buffer number
+---@param bufnr number buffer number
+---@return string name name of the buffer
+M.get_buf_name = function(bufnr)
+	return vim.api.nvim_buf_get_name(bufnr)
+end
+
+---@class Buffer
+---@field bufnr number buffer number
+---@field bufname string buffer name
+---@field window number window number of the buffer
+
+--- Get buffers withing given windows
+---@param windows any[] windows
+---@return Buffer[] buffers in given windows
+M.get_buffers_for_windows = function(windows)
+	windows = windows or {}
+	local buffers = {}
+	for _, window in ipairs(windows) do
+		local bufnr = vim.api.nvim_win_get_buf(window)
+		table.insert(buffers, {
+			bufnr = bufnr,
+			bufname = M.get_buf_name(bufnr),
+			window = window,
+		})
+	end
+
+	return buffers
+end
+
+---@class TabPage
+---@field handle number tab page file handle
+---@field tabnr number tab index number
+---@field windows number[] list of window handle numbers in tab
+
+--- Get list of `TabPage`
+---@return TabPage[] tab_pages list of tab tages currently available
+M.get_tabpages = function()
+	local handles = vim.api.nvim_list_tabpages()
+
+	local tabs = {}
+	for index, handle in ipairs(handles) do
+		table.insert(tabs, {
+			handle = handle,
+			tabnr = index,
+			windows = vim.api.nvim_tabpage_list_wins(handle),
+		})
+	end
+
+	return tabs
+end
+
+---@class Tab
+---@field tabnr number tab index number
+---@field current_page number current buffer number
+---@field current_page_name string current buffer name
+---@field buffers Buffer[] list of buffers in tab page
+
+--- Get currently available tabs
+---@return Tab[] tabs currently available tabs
+M.get_tabs = function()
+	local tabs = M.get_tabpages()
+
+	local pages = {}
+	for _, tabpage in ipairs(tabs) do
+		local current_bufnr = M.get_active_buf_for_tab(tabpage.handle)
+
+		if current_bufnr ~= -1 then
+			table.insert(pages, {
+				tabnr = tabpage.tabnr,
+				current_page = current_bufnr,
+				current_page_name = M.get_buf_name(current_bufnr),
+				buffers = M.get_buffers_for_windows(tabpage.windows),
+			})
+		end
+	end
+
+	return pages
+end
+
+---@class TabResult
+---@field tabnr number tab index number
+---@field bufnr number buffer number within the tab
+---@field bufname string buffer name within the tab
+---@field window number|nil window number of the tab
+
+--- Make tab results from given `tabs` based on given `opts`
+---@param opts table table of configuration options
+---@param tabs Tab[] currently available tabs
+---@return TabResult[] tab_results Telescope viewable results of the the tabs
+M.make_tab_results = function(opts, tabs)
+	---@type TabResult[]
+	local results = {}
+
+	if opts.settings and opts.settings.all_buffers == true then
+		for _, tabpage in ipairs(tabs) do
+			for _, tabbuffer in ipairs(tabpage.buffers) do
+				if tabbuffer.bufname ~= "" then
+					table.insert(results, {
+						tabnr = tabpage.tabnr,
+						bufnr = tabbuffer.bufnr,
+						bufname = tabbuffer.bufname,
+						window = tabbuffer.window,
+					})
+				end
+			end
+		end
+	else
+		for _, tabpage in ipairs(tabs) do
+			table.insert(results, {
+				tabnr = tabpage.tabnr,
+				bufnr = tabpage.current_page,
+				bufname = tabpage.current_page_name,
+			})
+		end
+	end
+
+	return results
+end
+
+---Get `TabResult` list that can be used in tab pickers
+---@param opts table|nil of optional configuration options
+---@return TabResult[] results containg tab results that can be used in pickers
+M.get_tab_results = function(opts)
+	local o = vim.tbl_deep_extend("force", {}, opts or tab_picker_default_settings)
+
+	return M.make_tab_results(o, M.get_tabs())
+end
+
+---@type TabResult[]
+local _results = {}
+
+---Find `TabResult` from `results` by `bufnr` of the tab buffer
+---@param bufnr number buffer number of the tab window
+---@param results TabResult[] tab results to look for buffer
+---@return number|nil index Found tab index or `nil` if `results` does not have suchs buffer
+---@return TabResult|nil result Found tab result or `nil` if `results` does not have such buffer
+M.find_tab_result_by_bufnr = function(bufnr, results)
+	local i, found = nil, nil
+	for index, tab in ipairs(results) do
+		if tab.bufnr == bufnr then
+			i = index
+			found = tab
+			break
+		end
+	end
+
+	return i, found
+end
+
+local function tab_files_picker(opts)
+	opts = vim.tbl_deep_extend("force", {}, themes.get_dropdown(opts or {}))
+	if tab_picker_sort_last_active == 0 then
+		_results = M.get_tab_results(opts)
+	end
+	local cwdlen = #vim.fn.getcwd()
+
+	pickers
+		.new(opts, {
+			prompt_title = "Tabs",
+			finder = finders.new_table({
+				results = _results,
+				entry_maker = function(tabpage)
+					local name = string.sub(tabpage.bufname, cwdlen + 1)
+
+					return {
+						value = tabpage,
+						display = "(" .. tabpage.tabnr .. ") ." .. name,
+						ordinal = tabpage.bufname,
+					}
+				end,
+			}),
+			sorter = conf.generic_sorter(opts),
+			attach_mappings = function(prompt_bufnr, _) -- map
+				telescope_actions.select_default:replace(function()
+					telescope_actions.close(prompt_bufnr)
+					local entry = telescope_actions_state.get_selected_entry()
+					if entry ~= nil then
+						local tab = entry.value.tabnr
+						vim.cmd("tabnext " .. tab)
+
+						if entry.value.window ~= nil then
+							vim.fn.win_gotoid(entry.value.window)
+						end
+					end
+				end)
+				return true
+			end,
+		})
+		:find()
+end
+
+vim.keymap.set("n", "<leader>tp", function()
+	tab_files_picker(tab_picker_default_settings)
+end, keymap_opts)
+
+if tab_picker_sort_last_active == 1 then
+	vim.api.nvim_create_autocmd({ "WinEnter" }, {
+		pattern = { "*" },
+		callback = function(opts)
+			local res = M.make_tab_results(tab_picker_default_settings, M.get_tabs())
+
+			for _, result in ipairs(res) do
+				local existing_index, existing_tab = M.find_tab_result_by_bufnr(result.bufnr, _results)
+				if existing_index ~= nil and existing_tab ~= nil then
+					table.remove(_results, existing_index)
+					if opts.buf == existing_tab.bufnr then
+						table.insert(_results, 1, result)
+					else
+						table.insert(_results, existing_index, result)
+					end
+				else
+					table.insert(_results, result)
+				end
+			end
+		end,
+	})
+end
+
+return M
