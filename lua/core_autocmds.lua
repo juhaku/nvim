afb414087b17dc592ea2d6fce699fcc8ec2b8da0
Juha Kukkonen <juha7kukkonen@gmail.com>
2023-11-24 18:29:08 +0200

    Fix autoremove non nexisting buffer files

--- lua/core_autocmds.lua
+++ lua/core_autocmds.lua
@@ -2,8 +2,9 @@ if require("global_options").auto_close_missing_buffers == true then
 	vim.api.nvim_create_autocmd({ "FileChangedShellPost" }, {
 		pattern = "*",
 		callback = function(o)
-			local exists = vim.fn.system("test -f " .. o.match .. " && echo true")
-			if exists ~= "true" then
+			local exists = vim.fn.system("test -f " .. o.match .. " && echo 1")
+			if tonumber(exists) ~= 1 then
+				vim.notify("file: " .. o.match .. " does not exists", vim.log.levels.INFO)
 				local wins = vim.fn.win_findbuf(o.buf)
 				for _, win in ipairs(wins) do
 					vim.api.nvim_win_close(win, true)
