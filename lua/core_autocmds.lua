c11deeba4e62c25586d8bfb3ddd5ba7f51380e35
Juha Kukkonen <juha7kukkonen@gmail.com>
2024-01-26 00:55:43 +0200

    Add autorotate lsp.log file when above 10mb

--- lua/core_autocmds.lua
+++ lua/core_autocmds.lua
@@ -13,3 +13,19 @@ if require("global_options").auto_close_missing_buffers == true then
 		end,
 	})
 end
+
+local lsp_log = os.getenv("HOME") .. "/.local/state/nvim/lsp.log"
+
+vim.api.nvim_create_autocmd({"VimEnter"}, {
+    pattern = "*",
+    callback = function ()
+        local size_in_bytes = vim.fn.system("stat --format=%s " .. lsp_log)
+        if tonumber(size_in_bytes) > 10 * 1024 * 1024 then
+            vim.notify("lsp.log is bigger than 10Mb, rotating file", vim.log.levels.INFO)
+            local date = vim.fn.system("date --iso-8601=date")
+            vim.fn.system("mv " .. lsp_log .. " " .. lsp_log .. ".old." .. date)
+            vim.fn.system("touch" .. lsp_log)
+            vim.fn.system("fd lsp.log.old --older 3months" .. os.getenv("HOME") .. "/.local/state/nvim --exec rm -f {}")
+        end
+    end
+})
