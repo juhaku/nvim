56401d26f1762bf95006eda2ffae9bc33e483a70
Juha Kukkonen <juha7kukkonen@gmail.com>
2024-04-17 01:49:54 +0300

    Enable fluttertools debug and prettier formatting

--- lua/juhaku/plugins/fluttertools.lua
+++ lua/juhaku/plugins/fluttertools.lua
@@ -6,20 +6,20 @@ return {
 	},
 	config = true,
 	opts = {
-		-- debugger = {
-		-- 	enabled = true,
-		-- 	register_configurations = function()
-		-- 		require("dap").configurations.dart = {
-		-- 			{
-		-- 				name = "Flutter",
-		-- 				request = "launch",
-		-- 				type = "dart",
-		-- 				flutterMode = "debug",
-		-- 			},
-		-- 		}
-		-- 		require("dap.ext.vscode").load_launchjs()
-		-- 	end,
-		-- },
+		debugger = {
+			enabled = true,
+			register_configurations = function()
+				require("dap").configurations.dart = {
+					{
+						name = "Flutter",
+						request = "launch",
+						type = "dart",
+						flutterMode = "debug",
+					},
+				}
+				require("dap.ext.vscode").load_launchjs()
+			end,
+		},
 		lsp = {
 			on_attach = require("juhaku.plugins.lsp").on_attach,
 			handlers = require("juhaku.plugins.lsp").handlers,
--- lua/juhaku/plugins/nonels.lua
+++ lua/juhaku/plugins/nonels.lua
@@ -5,20 +5,18 @@ return {
 
 		local sources = {
 			-- null_ls.builtins.formatting.prettier,
-			-- null_ls.builtins.formatting.prettierd, -- TODO
+			null_ls.builtins.formatting.prettierd,
 			-- null_ls.builtins.diagnostics.eslint,
 			-- null_ls.builtins.code_actions.eslint,
 			-- null_ls.builtins.diagnostics.eslint_d,
 			-- null_ls.builtins.code_actions.eslint_d,
 			-- null_ls.builtins.formatting.eslint_d,
 			null_ls.builtins.formatting.stylua,
-			-- null_ls.builtins.formatting.beautysh, -- TOOD
-			-- null_ls.builtins.code_actions.shellcheck, -- TODO
 			null_ls.builtins.formatting.goimports, -- TODO
 			null_ls.builtins.formatting.gofumpt, -- TODO
 			null_ls.builtins.diagnostics.staticcheck, -- TODO
-			-- null_ls.builtins.formatting.markdownlint, -- TODO
 			null_ls.builtins.code_actions.gitsigns, -- TODO
+			null_ls.builtins.formatting.shfmt,
 			-- require("typescript.extensions.null-ls.code-actions"), -- TODO
 		}
 
